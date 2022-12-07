1e46bb8bf0212abeaddcc1d66352e5d6e965d394
Witek Bobrowski <witek@bobrowski.com.pl>
2022-12-07 18:42:49 +0100

    Add solutions to Day 7

---
+++ day-07.swift
@@ -0,0 +1,54 @@
+// --- Day 7: No Space Left On Device ---
+// https://adventofcode.com/2022/day/7
+import Foundation
+
+private func readInput(from path: String) throws -> String {
+    let url = URL(fileURLWithPath: path)
+    return try String(contentsOf: url, encoding: .utf8)
+}
+private let terminalOutput = try readInput(from: "input/day-07.txt")
+
+private func traverseFilesystem(_ terminalOutput: String) -> [String: Int] {
+    var current = [String]()
+    var sizes = [String: Int]()
+    terminalOutput.enumerateLines { line, _ in
+        if line.first == "$" {
+            let command = line.components(separatedBy: " ").dropFirst()
+            guard command.first == "cd" else { return }
+            guard command.last == ".." else {
+                return current.append(command.last ?? "")
+            }
+            current.removeLast()
+        } else if let size = Int(line.prefix { $0 != " " }) {
+            _ = current.reduce([String]()) { path, current in
+                sizes[(path + [current]).joined(separator: "-"), default: 0] += size
+                return path + [current]
+            }
+        }
+    }
+    return sizes
+}
+
+// MARK: - Part One
+print("--- Day 7: No Space Left On Device ---")
+
+private func disposableSpace(_ terminalOutput: String) -> Int {
+    let sizes = traverseFilesystem(terminalOutput)
+    return sizes.filter { $1 <= 100000 }.values.reduce(0, +)
+}
+
+print("Solution:", disposableSpace(terminalOutput))
+// Solution: 1581595
+
+// MARK: - Part Two
+print("--- Part Two ---")
+
+private func disposableSpaceWithRequirements(_ terminalOutput: String) -> Int {
+    let sizes = traverseFilesystem(terminalOutput)
+    return sizes
+        .filter { (70000000 - sizes["/"]!) + $1 > 30000000 }
+        .min { $0.1 < $1.1 }?.1 ?? 0
+}
+
+print("Solution:", disposableSpaceWithRequirements(terminalOutput))
+// Solution: 1544176
